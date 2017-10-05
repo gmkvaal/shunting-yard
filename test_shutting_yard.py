6ce4b4bb01e56048c7591019c668269388853a2a
Guttorm Kvaal <guttorm.kvaal@gmail.com>
2017-10-05 17:27:00 +0200

    removed deleted files from git

--- test_shutting_yard.py
+++
@@ -1,9 +0,0 @@
-from shutting_yard import Shutting_yard
-
-
-def test_input_string():
-
-    innstr = "1+2+33"
-    sy = Shutting_yard(innstr)
-
-    assert len(innstr) == len(sy.split_input_string())

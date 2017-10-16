899b6aae95a7f7549db1d4dddd40f5c3f8c8405d
Guttorm Kvaal <guttorm.kvaal@gmail.com>
2017-10-16 16:22:48 +0200

    added test for parser. passed

---
+++ shunting-yard/test/test_parser.py
@@ -0,0 +1,19 @@
+from Parser import parser
+
+def test_input_string1():
+    """ Testing parser function """
+
+    innstr = 'a+b+c'
+    correct_output = ['a', '+', 'b', '+', 'c']
+    output_list = parser(innstr)
+
+    assert output_list == correct_output
+
+def test_input_string1():
+    """ Testing parser function """
+
+    innstr = 'sin(a*b+cos(c))'
+    correct_output = ['sin', '(', 'a', '*', 'b', '+', 'cos', '(', 'c', ')', ')']
+    output_list = parser(innstr)
+
+    assert output_list == correct_output

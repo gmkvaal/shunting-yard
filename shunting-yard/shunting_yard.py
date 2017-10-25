f02d5787a25cc11c75d7d53df7d7b94daa5cb360
Guttorm Kvaal <guttorm.kvaal@gmail.com>
2017-10-25 17:09:59 +0200

    Fixed some PEP8 stuff and added test_parser that had disappeared to another branch

--- shunting-yard/shunting_yard.py
+++ shunting-yard/shunting_yard.py
@@ -1,4 +1,29 @@
 from parser_FSM import tokenizer
+from typing import List
+from collections import namedtuple
+
+
+StateRet = namedtuple('StateRet', ['next_state', 'done', 'increment'])
+
+
+def numbers(char: str, stack: List[str], output_queue: List[str]) -> StateRet:
+
+    pass
+
+
+def operators(char: str, stack: List[str], output_queue: List[str]) -> StateRet:
+
+    pass
+
+
+def left_bracket(char: str, stack: List[str], output_queue: List[str]) -> StateRet:
+
+    pass
+
+
+def left_bracket(char: str, stack: List[str], output_queue: List[str]) -> StateRet:
+
+    pass
 
 
 if __name__ == '__main__':
